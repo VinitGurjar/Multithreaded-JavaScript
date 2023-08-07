b229b507d2b5b6ecaa72191069c9d050f4306bed
adhiramaa <vinitfagna@gmail.com>
2023-08-07 23:56:58 +0530

    Implement node.js worker module

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+const { Worker } = require("workers_threads");
+
+function chunkify(array, n) {
+  let chunks = [];
+  for (let i = n; i > 0; i--) {
+    chunks.push(array.splice(0, Math.ceil(array.length / i)));
+  }
+  return chunks;
+}
+
+function run(jobs, concurrentWorkers) {
+  const chunks = chunkify(jobs, concurrentWorkers);
+  chunks.forEach((chunk) => {
+    const worker = new Worker("./worker.js");
+    worker.postMessage(data);
+  });
+}
+
+/* old code
+
 //Simulating a CPU intensive task that would block the main thread
 
 //populating array with the number 1 billion 100times
-const jobs = Arrray.from({ length: 100 }, () => 1e9);
+const jobs = Array.from({ length: 100 }, () => 1e9);
 
 //Using performance module to grab the timestamp before and after this code runs to measure the execution time
 //Performance.now() returns the number of milliseconds since the UNIX epoch
 const tick = performance.now();
 
-//count to 1 billion
-for (let job of jobs) {
-  let count = 0;
-  for (let i = 0; i < jobs; i++) {
-    count++;
-  }
-}
-
 const tock = performance.now();
 
 console.log(`Main thread took ${tock - tick} milliseconds to complete`);
+
+*/
